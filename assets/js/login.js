c00bed26e8a1fcdbd5d7f11bdd583dec01497ab0
hushenhui <347795721@qq.com>
2020-12-30 16:58:11 +0800

    feat:首页功能完成

---
+++ assets/js/login.js
@@ -0,0 +1,74 @@
+$(function () {
+    //去注册账号
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    //去登录
+    $('#link_login').on('click', function () {
+        $('.reg-box').hide()
+        $('.login-box').show()
+    })
+
+    //密码正则验证
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        // 自定义了一个 pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能为空'],
+        //验证2次密码是否一致
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+
+    //监听注册表单提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        // 2. 发起Ajax的POST请求
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            // 模拟点击行为
+            $('#link_login').click()
+
+        })
+    })
+
+      // 监听登录表单的提交事件
+      $('#form_login').submit(function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
+
+
+})
